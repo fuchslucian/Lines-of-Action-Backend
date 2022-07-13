4bc62cb12e1b71f41f2928f1d2e1017786cf7cfd
fuchslucian <80114645+fuchslucian@users.noreply.github.com>
2022-07-13 14:31:31 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(http, {cors: {origin: '*'}});
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 
